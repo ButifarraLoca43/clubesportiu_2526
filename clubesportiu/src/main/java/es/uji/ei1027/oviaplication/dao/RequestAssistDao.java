3f5fb1079ff052a2a8ea03c1ea522f916f6c3327
NestaRB <victornestareig@gmail.com>
2026-04-22 20:04:19 +0200

    Inicio Request_for_pap_pati

---
+++ clubesportiu/src/main/java/es/uji/ei1027/oviaplication/dao/RequestAssistDao.java
@@ -0,0 +1,72 @@
+package es.uji.ei1027.oviaplication.dao;
+
+import es.uji.ei1027.oviaplication.model.Match;
+import es.uji.ei1027.oviaplication.model.RequestAssist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RequestAssistDao {
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void addRequestAssist(RequestAssist requestAssist) {
+        jdbcTemplate.update("INSERT INTO request_for_pap_pati (iduser, date, ,requiredsupport, description, requirements, lifeproject) VALUES (?, ?, ?, ?, ?, ?)",
+                requestAssist.getIduser(),
+                requestAssist.getDate(),
+                requestAssist.getRequiredsupport(),
+                requestAssist.getDescription(),
+                requestAssist.getRequirements(),
+                requestAssist.getLifeproject()
+        );
+    }
+
+    public void deleteRequestAssist(RequestAssist requestAssist)
+    {
+        jdbcTemplate.update("DELETE FROM request_for_pap_pati WHERE idnumber =?",
+                requestAssist.getIdnumber()
+        );
+    }
+
+    public void updateRequestAssist(RequestAssist requestAssist)
+    {
+        jdbcTemplate.update("UPDATE request_for_pap_pati  SET iduser=?, date=?, requiredsupport=?, description=?, requirements=?, lifeproject=? WHERE idnumber=?",
+                requestAssist.getIduser(),
+                requestAssist.getDate(),
+                requestAssist.getRequiredsupport(),
+                requestAssist.getDescription(),
+                requestAssist.getRequirements(),
+                requestAssist.getLifeproject(),
+                requestAssist.getIdnumber()
+        );
+    }
+
+    public RequestAssist getRequestAssist(String idnumber) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * FROM request_for_pap_pati WHERE idnumber=?",
+                    new RequestAssistRowMapper(),
+                    idnumber
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    public List<RequestAssist> getRequestAssists() {
+        try {
+            return jdbcTemplate.query("SELECT * FROM request_for_pap_pati", new RequestAssistRowMapper()
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return new ArrayList<>();
+        }
+    }
+}
---
+++ clubesportiu/src/main/java/es/uji/ei1027/oviaplication/dao/RequestAssistRowMapper.java
@@ -0,0 +1,24 @@
+package es.uji.ei1027.oviaplication.dao;
+
+import es.uji.ei1027.oviaplication.model.Match;
+import es.uji.ei1027.oviaplication.model.RequestAssist;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+public class RequestAssistRowMapper  implements RowMapper<RequestAssist> {
+    @Override
+    public RequestAssist mapRow(ResultSet rs, int rowNum) throws SQLException {
+        RequestAssist requestAssist = new RequestAssist();
+        requestAssist.setIduser(rs.getString("iduser"));
+        requestAssist.setDate(rs.getObject("date", LocalDate.class));
+        requestAssist.setRequiredsupport(rs.getString("requiredsupport"));
+        requestAssist.setDescription(rs.getString("description"));
+        requestAssist.setRequirements(rs.getString("requirements"));
+        requestAssist.setLifeproject(rs.getString("lifeproject"));
+
+        return requestAssist;
+    }
+}
